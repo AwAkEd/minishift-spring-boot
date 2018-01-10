cfaa4cb8b76ce76107e554e0f090c4680f4c3c21
Javier Serrano <javi.serrano@gmail.com>
2018-01-10 16:46:42 +0100

    test changes

--- src/main/java/com/entelgydigital/minishift/MinishiftDemoApplication.java
+++ src/main/java/com/entelgydigital/minishift/MinishiftDemoApplication.java
@@ -1,12 +1,23 @@
-package com.entelgydigital.minishift;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class MinishiftDemoApplication {
+public class MinishiftDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MinishiftDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MinishiftDemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MinishiftDemoApplication.class);
+    }
 }
