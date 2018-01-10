c9524a794eb068cfa0afe97a2ba4358f6f3bd071
Javier Serrano <javi.serrano@gmail.com>
2018-01-10 16:40:21 +0100

    removed unnecessary annotations

--- src/main/java/com/entelgydigital/minishift/MinishiftDemoApplication.java
+++ src/main/java/com/entelgydigital/minishift/MinishiftDemoApplication.java
@@ -1,15 +1,8 @@
 package com.entelgydigital.minishift;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class MinishiftDemoApplication {
 
